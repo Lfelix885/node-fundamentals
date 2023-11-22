e2f90c8e10412b22df42664f0502586052da6b8c
leonardo_felix <lfelix885@gmail.com>
2023-11-22 19:41:33 -0300

    **[Structure and Streams] - POST and GET methods;
    **[BUFFERS] - Fake upload to streams

--- src/server.js
+++ src/server.js
@@ -2,26 +2,39 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
-    const { method, url } = req;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-    if (method === 'GET' && url === '/users') {
-        return res
-            .setHeader('Content-Type', 'application/json')
-            .writeHead(200)
-            .end(JSON.stringify(users));
-    }
+  const buffers = [];
 
-    if (method === 'POST' && url === '/users') {
-        users.push({
-            id: 1,
-            name: 'John Doe',
-            email: 'Johndoe@example.com',
-        });
-        return res.writeHead(201).end();
-    }
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
 
-    return res.writeHead(404).end('Not found!');
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    req.body = null;
+  }
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .writeHead(200)
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { nome, email } = req.body;
+    users.push({
+      id: 1,
+      nome,
+      email,
+    });
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end('Not found!');
 });
 
 server.listen(3333);

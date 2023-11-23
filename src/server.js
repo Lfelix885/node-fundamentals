7f1bd735973907f5294e0ec9791992dccea2e503
leonardo_felix <lfelix885@gmail.com>
2023-11-22 23:51:27 -0300

    **[Dinamical query params] - Dinamically receiving query params and filtering it with regular expressions

--- src/server.js
+++ src/server.js
@@ -2,15 +2,24 @@ import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
 
+// QUERY PARAMETERS => URL Statefull => Filtros, paginação; não são obrigatórios
+// ROUTE PARAMETERS => Identificação de recurso
+// REQUEST BODY => Envio de infos de um formulário (HTTPS)
+
+// GET https://localhost:3333/users?userId=1
+// DELETE https://localhost:3333/users/1
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
   const route = routes.find(
-    (route) => route.method === method && route.path === url
+    (route) => route.method === method && route.path.test(url)
   );
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+    console.log(routeParams);
     return route.handler(req, res);
   }
 
